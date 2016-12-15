a4fb76525b1fb62ee420060e0f55180ec9acfe09
fpkerckh <frederiekmaarten.kerckhof@ugent.be>
2016-12-15 13:05:34 +0100

    add code and data Mélanie

---
+++ 161215-QPCR Workshop-Rscript.R
@@ -0,0 +1,19 @@
+rm (list = ls())
+library(ade4TkGUI)
+library(grofit)
+library(readxl)
+#import data as dataframe
+# ade4TkGUI()
+#   ## "Read a data file"_no header
+#   ## Copy "QPCR-time"-Import from clipboard in dataframe "time1"
+#   ## Copy "QPCR-data"-Import from clipboard in dataframe "data1"
+# time1
+# data1
+
+data1 <- readxl::read_excel("161216-Data sets 1 species.xls",sheet="QPCR-data",col_names = FALSE)
+time1 <- readxl::read_excel("161216-Data sets 1 species.xls",sheet="QPCR-time",col_names = FALSE)
+
+
+MyOptL<-grofit.control(fit.opt="m",model.type=c("gompertz"),log.y.gc=TRUE, interactive = FALSE)
+tableresults<-grofit(time1,data1,FALSE,MyOptL)$gcFit$gcTable
+print(tableresults)
