b9363a894bfc204dab56820ee32de43154249855
fpkerckh <frederiekmaarten.kerckhof@ugent.be>
2016-12-15 14:22:00 +0100

    added automated export

--- 161215-QPCR Workshop-Rscript.R
+++ 161215-QPCR Workshop-Rscript.R
@@ -2,6 +2,7 @@ rm (list = ls())
 library(ade4TkGUI)
 library(grofit)
 library(readxl)
+library(xlsx) #requires Java on your PC!
 #import data as dataframe
 # ade4TkGUI()
 #   ## "Read a data file"_no header
@@ -17,3 +18,7 @@ time1 <- readxl::read_excel("161216-Data sets 1 species.xls",sheet="QPCR-time",c
 MyOptL<-grofit.control(fit.opt="m",model.type=c("gompertz"),log.y.gc=TRUE, interactive = FALSE)
 tableresults<-grofit(time1,data1,FALSE,MyOptL)$gcFit$gcTable
 print(tableresults)
+write.csv2(x = tableresults,file = "tableresults.csv") #write.csv2 is for semicolon separated files (, as decimal separator)
+
+#### if you have Java on your machine you can write directly to the excel file ####
+write.xlsx(tableresults,"161216-Data sets 1 species.xls",sheetName = "Eff2",append = TRUE)
